cd4a38afe806e69c09bcf2da4fc0e6b67c9bf313
wildan <wildan78933@gmail.com>
2024-02-28 07:04:22 +0700

    update

--- app/config.js
+++ app/config.js
@@ -2,7 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  urlDb: process.env.URL_MONGODB_DEV,
+  // urlDb: process.env.URL_MONGODB_DEV,
+  urlDb: process.env.URL_MONGODB_PRO,
   jwtExpiration: process.env.JWT_EXPIRATION,
   jwtSecret: process.env.JWT_SECRET_KEY,
   // jwtRefreshTokenExpiration: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
